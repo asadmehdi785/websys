92e94a0b9d26d6af2480cd15793e12e7d5c4ff2e
Asad <mehdia@rpi.edu>
2016-11-14 22:38:20 -0500

    updated files with jquery ui

--- Resources/js/homework.js
+++ Resources/js/homework.js
@@ -9,8 +9,7 @@ $(document).ready(function() {
 				var name = $(this).find('name').text();
 				var description = $(this).find('description').text();
 				var location = $(this).find('location').text();
-				$('#homework').append('<div class="row"><div class="col-md-12"><h3>' + name + '</h3>' + '<p>' + description + '</p>' + '<a class="btn btn-primary" href="' + location + '">View Project <span class="glyphicon glyphicon-chevron-right"></span></a></div></div>');
-				
+				$('#homework').append('<div class="row"><div class="col-md-12"><h3>' + name + '</h3>' + '<p>' + description + '</p>' + '<a class="ui-button ui-widget ui-corner-all" href="' + location + '">View Project <span class="glyphicon glyphicon-chevron-right"></span></a></div></div>');
 			})
 		}
 	});
